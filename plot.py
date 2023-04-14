d7b1c188543ce9eb9dda1262be8cdb42d5226efe
jonasbarth <jonas.barth.95@gmail.com>
2023-04-14 18:21:16 +0200

    #3 algorithm complexity * fix incorrect module name * create plot function for connectivity probability * improve argument names in graph creation functions

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import complexity
+import connectivity
 
 
 def plot_time_complexity(time_complexity: list[complexity.TimeComplexity], labels: [str], title: str):
@@ -22,4 +23,24 @@ def plot_time_complexity(time_complexity: list[complexity.TimeComplexity], label
     plt.ylabel("Time in s")
     plt.xlabel("Number of nodes in graph")
     plt.legend()
+    plt.show()
+
+
+def plot_connectivity_prob(connectivity_prob: list[connectivity.Connectivity], labels: [str], title: str, xlabel: str):
+    """Plots the connectivity.
+
+    :arg
+    connectivity_prob - a list of Connectivity named tuples.
+    labels - a list of labels.
+    title - the title of the plot.
+    """
+    for connec, label in zip(connectivity_prob, labels):
+        plt.plot(connec.x, connec.probs, label=label)
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel("Connectivity Probability")
+    if len(connectivity_prob) > 1:
+        plt.legend()
+
     plt.show()
