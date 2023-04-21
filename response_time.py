19098ee88455cad1345188481b3029a935651c23
martinezvelascojavier <113454044+martinezvelascojavier@users.noreply.github.com>
2023-04-21 16:22:20 +0200

    Update response_time.py

--- response_time.py
+++ response_time.py
@@ -25,33 +25,3 @@ def simulate_response_time(n_servers):
         exec_times[i] += outbound_time + return_time
 
     return np.max(exec_times)
-
-
-if __name__ == "__main__":
-
-    N = 10000
-    n = 64
-    tau_s = 0.000005
-    capacity_gbit = 10
-    expected_job_time_s = 8 * 3600
-    fixed_job_time_s = 30
-    input_file_size_gb = 4000
-    output_file_size_gb = 4000
-    overhead = 1 + 48 / 1500
-
-    baseline = np.random.exponential(expected_job_time_s) + fixed_job_time_s
-
-    tree = fat_tree.FatTree(n, tau_s, capacity_gbit)
-    response_times = []
-
-    servers = range(1, N + 1, 10)
-
-    for n_servers in tqdm(servers):
-        response_time = np.mean([simulate_response_time(n_servers) for _ in range(10)])
-        response_times.append(response_time)
-
-    plt.plot(servers, np.array(response_times) / baseline)
-    plt.xlabel("Number of Servers")
-    plt.ylabel("Response Time")
-    plt.title("Normalised Response Time")
-    plt.show()
